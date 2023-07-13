095e168eab41b4d2b5c50a927a991921f4ac2e44
John Stanley Altonaga <johntaltonaga@gmail.com>
2023-07-13 22:03:09 +0800

    Create category page

---
+++ src/pages/Women.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "../styles/Women.css";
+
+export default function Women() {
+  return (
+    <>
+      <div className="hero bg-image">
+        <p className="hero-text display-1">Women</p>
+      </div>
+      <Container>
+        <Col></Col>
+      </Container>
+    </>
+  );
+}
